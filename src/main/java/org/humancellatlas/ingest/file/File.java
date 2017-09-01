84a152b7e8c5801c18bbf6bc3f2f63c23270513e
Rolando Fernandez <rolando@ebi.ac.uk>
2017-09-01 14:53:27 +0100

    added File repository (for tracking link between our metadata entities and datastore "file" resources)

--- src/main/java/org/humancellatlas/ingest/file/File.java
+++ src/main/java/org/humancellatlas/ingest/file/File.java
@@ -1,7 +1,21 @@
 package org.humancellatlas.ingest.file;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Getter;
+
+import java.util.UUID;
+
 /**
  * Created by rolando on 01/09/2017.
  */
+@Getter
 public class File {
+    private String id;
+    private String metadataUUID;
+
+    @JsonCreator
+    public File(){
+        this.id = UUID.randomUUID().toString();
+        metadataUUID = "";
+    }
 }
