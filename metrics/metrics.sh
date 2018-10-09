a950df08e2523e1e13a291249c238b9861b83915
Rodrey Goite <rodrey@ebi.ac.uk>
2018-10-09 11:30:18 +0100

    Update metrics script to write results to a file in METRICS_DIR.

--- metrics/metrics.sh
+++ metrics/metrics.sh
@@ -4,15 +4,23 @@ if [ -z "${METRICS_PREFIX}" ]; then
   METRICS_PREFIX=ingest_core
 fi
 
+if [ -z "${METRICS_DIR}" ]; then
+  METRICS_DIR=$PWD/metrics
+fi
+
 function count_fd
 {
   PID=$(jcmd -l | awk 'NR==1 {print $1}')
   FD_COUNT=$(lsof -p ${PID} | wc -l)
-  echo $FD_COUNT
+  echo ${FD_COUNT}
 }
 
-function fd_prom {
-  echo "${METRICS_PREFIX}_open_fd $(count_fd) $(date +%s)"
+function log_fd_count {
+  if [ ! -f ${METRICS_DIR} ]; then
+    mkdir -p ${METRICS_DIR}
+  fi
+  ENTRY="${METRICS_PREFIX}_open_fd $(count_fd) $(date +%s)"
+  echo ${ENTRY} > ${METRICS_DIR}/fd_count.prom
 }
 
-fd_prom
+log_fd_count
